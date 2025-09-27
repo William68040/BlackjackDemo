dcbcee3e839d7bdbbd88bbe9136728633a8370e2
William Luc <williamluc888@outlook.com>
2025-09-27 20:30:09 +0200

    Added functions to the "Deck" class to create a list to hold cards, create a function for a random number generator (for shuffling the cards)
    And finally a function to create a deck consisting of 52 cards with all suits and ranks

--- BlackjackDemo/Deck.cs
+++ BlackjackDemo/Deck.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace BlackjackDemo
 {
-    internal class Deck
+    public class Deck
     {
+        // List to hold the cards in the deck
+        private List<Card> cards;
+
+        // Random number generator for shuffling the deck
+        private Random Random;
+
+        public Deck()
+        {
+            foreach (Enums.Suit suit in Enum.GetValues(typeof(Enums.Suit))) // Retrieves the values of the Suit enumeration
+            {
+                foreach (Enums.Rank rank in Enum.GetValues(typeof(Enums.Rank))) // Retrieves the values of the Rank enumeration 
+                {
+                    cards.Add(new Card(suit, rank)); // Creates a new Card object with the current suit and rank, and adds it to the cards list
+                }
+            }
+        }
+
+
     }
 }
