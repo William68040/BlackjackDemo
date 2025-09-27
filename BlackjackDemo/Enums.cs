6b439118820218990ef495f326004c878457f14f
William Luc <williamluc888@outlook.com>
2025-09-27 19:50:05 +0200

    Added Enumerations for the card suites and ranks in a standard deck of 52 cards

--- BlackjackDemo/Enums.cs
+++ BlackjackDemo/Enums.cs
@@ -8,5 +8,33 @@ namespace BlackjackDemo
 {
     internal class Enums
     {
+        // Enumeration for Card Suits and Ranks in a standard deck of cards of 52 cards
+
+        public enum Suit
+        {
+            Hearts,
+            Diamonds,
+            Clubs,
+            Spades
+
+        }
+
+        public enum Rank
+        {
+            Two = 2,
+            Three = 3,
+            Four = 4,
+            Five = 5,
+            Six = 6,
+            Seven = 7,
+            Eight = 8,
+            Nine = 9,
+            Ten = 10,
+            Jack = 10,
+            Queen = 10,
+            King = 10,
+            Ace = 11
+        }
+
     }
 }
