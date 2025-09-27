1520391f702ce0d1044d85b25f31b2345458ebf0
William Luc <williamluc888@outlook.com>
2025-09-27 20:02:41 +0200

    Added readable information about the code in the class "Card"

--- BlackjackDemo/Card.cs
+++ BlackjackDemo/Card.cs
@@ -9,13 +9,18 @@ namespace BlackjackDemo
 {
     public class Card
     {
+        // Properties for Suit and Rank of the card, excluding "set" to make them inmutable
         public Enums.Suit Suit { get; }
         public Enums.Rank Rank { get; }
+
+        // Constructor to initialize a card with a specific suit and rank
         public Card(Enums.Suit suit, Enums.Rank rank)
         {
             Suit = suit;
             Rank = rank;
         }
+
+        // Returns a string representation of the card, e.g., "Ace of Spades"
         public override string ToString()
         {
             return $"{Rank} of {Suit}";
