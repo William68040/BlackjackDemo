b47e27cf906df6e885417459cbaadc183d25ccd4
willi <willi@DESKTOP-UB10NUM>
2025-09-27 19:37:03 +0200

    Add project files.

---
+++ BlackjackDemo/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace BlackjackDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           // This is a demo project for a Blackjack game.
+
+        }
+    }
+}
