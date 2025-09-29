389ea662d4d30446b366ff12538b5d58bb8b7865
William Luc <williamluc888@outlook.com>
2025-09-29 16:21:02 +0200

    Added functions to the "Hand" Class
    Added basic functions to create a list to hold the cards in the hand, draw a card
    
    Added methods to get values of the card & to convert an Ace from 11 to 1 if total value exceeds 21
    
    Added 3 booleans to check if hand is busted, to check if you have blackjack and if the Ace is soft ( if there is an ace that still counts as an 11 in the final value)

--- BlackjackDemo/Hand.cs
+++ BlackjackDemo/Hand.cs
@@ -6,7 +6,61 @@ using System.Threading.Tasks;
 
 namespace BlackjackDemo
 {
-    internal class Hand
+    public class Hand
     {
+        private List<Card> cards; // List to hold the cards in the hand
+
+        public Hand()
+        {
+            cards = new List<Card>(); // Initialize the list of cards
+        }
+
+        public void AddCard(Card card)
+        {
+            cards.Add(card); // Add a card to the hand
+        }
+
+        public int GetValue()
+        {
+            int value = 0;
+            int aceCount = 0;
+            foreach (Card card in cards)
+            {
+                if (card.Rank == Enums.Rank.Ace)
+                {
+                    value++; // Initially count Ace as 1
+                    aceCount++; // Keep track of the number of Aces
+                }
+                else
+                {
+                    value += (int)card.Rank; // Add the value of the card to the total
+                }
+
+                while (value > 21 && aceCount > 0)
+                {
+                    value -= 10; // Convert an Ace from 11 to 1 if the total value exceeds 21
+                    aceCount--; // Decrease the count of Aces that can be converted
+                }
+            }
+            return value; // Return the total value of the hand
+
+        }
+
+        public bool IsBusted()
+        {
+            return GetValue() > 21; // Check if the hand's value exceeds 21
+        }
+
+        public bool IsBlackjack()
+        {
+            return cards.Count == 2 && GetValue() == 21; // Check if the hand is a blackjack (two cards totaling 21)
+        }
+
+        public bool IsSoft()
+        {
+            return cards.Any(card => card.Rank == Enums.Rank.Ace) && GetValue() <= 21; // Check if the hand contains an Ace counted as 11 without busting
+        }
+
     }
+
 }
