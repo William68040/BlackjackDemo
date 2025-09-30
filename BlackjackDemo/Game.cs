0004d07735bd3b6f1a22834ce3390dd408bb3995
William Luc <williamluc888@outlook.com>
2025-09-30 11:25:49 +0200

    Added basic functions to the "Game" class
    Added a method to reset dealer and player hand and starting code for playing the round

--- BlackjackDemo/Game.cs
+++ BlackjackDemo/Game.cs
@@ -1,12 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BlackjackDemo
 {
-    internal class Game
+    public class Game
     {
+        private Deck deck;
+        private Hand playerHand;
+        private Hand dealerHand;
+        public void Play()
+        {
+            Console.WriteLine("The game is starting...");
+
+            bool continuePlaying = true;
+
+            while (continuePlaying)
+            {
+                PlayRound();
+            }
+
+        private void PlayRound() 
+        {
+            ResetHands();
+        }
+
+        public void ResetHands()
+        {
+            playerHand = new Hand(); // Reset player hand for a new round
+            dealerHand = new Hand();  // Reset dealer hand for a new round
+        }
+
+
+
     }
 }
